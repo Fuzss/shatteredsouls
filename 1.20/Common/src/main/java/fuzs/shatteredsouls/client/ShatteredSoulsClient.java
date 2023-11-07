2c3d1e8a62996b91d0001fb2fae684d64f4b9b67
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-11-08 00:02:25 +0100

    Update ShatteredSoulsClient.java

--- 1.20/Common/src/main/java/fuzs/shatteredsouls/client/ShatteredSoulsClient.java
+++ 1.20/Common/src/main/java/fuzs/shatteredsouls/client/ShatteredSoulsClient.java
@@ -3,6 +3,7 @@ package fuzs.shatteredsouls.client;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
 import fuzs.puzzleslib.api.client.core.v1.context.ParticleProvidersContext;
 import fuzs.puzzleslib.api.client.event.v1.RenderLivingEvents;
+import fuzs.puzzleslib.api.core.v1.ContentRegistrationFlags;
 import fuzs.puzzleslib.api.event.v1.entity.living.LivingDeathCallback;
 import fuzs.puzzleslib.api.event.v1.entity.living.LivingEvents;
 import fuzs.shatteredsouls.client.handler.MobDismembermentHandler;
@@ -27,5 +28,10 @@ public class ShatteredSoulsClient implements ClientModConstructor {
     @Override
     public void onRegisterParticleProviders(ParticleProvidersContext context) {
         context.registerClientParticleProvider(BloodParticle.BLOOD_PARTICLE_TYPE, BloodParticle.Provider::new);
+    }
+
+    @Override
+    public ContentRegistrationFlags[] getContentRegistrationFlags() {
+        return new ContentRegistrationFlags[]{ContentRegistrationFlags.CLIENT_PARTICLE_TYPES};
     }
 }
