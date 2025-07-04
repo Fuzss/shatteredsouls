f5e6d43a09719e7b1d84bff4fea2e8f12c432334
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-07-04 18:12:51 +0200

    full 1.21.7 port

--- 1.21.7/NeoForge/src/main/java/fuzs/shatteredsouls/neoforge/client/ShatteredSoulsNeoForgeClient.java
+++ 1.21.7/NeoForge/src/main/java/fuzs/shatteredsouls/neoforge/client/ShatteredSoulsNeoForgeClient.java
@@ -4,7 +4,7 @@ import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
 import fuzs.puzzleslib.neoforge.api.data.v2.core.DataProviderHelper;
 import fuzs.shatteredsouls.ShatteredSouls;
 import fuzs.shatteredsouls.client.ShatteredSoulsClient;
-import fuzs.shatteredsouls.neoforge.data.client.ModParticleProvider;
+import fuzs.shatteredsouls.data.client.ModParticleProvider;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
 
